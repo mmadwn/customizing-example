de25c961702bd232e2ec1156bdf653b3428073d3
mmadwn <muh.rdwan.u@gmail.com>
2024-04-18 13:54:09 +0700

    useContext

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,9 +1,6 @@
 import { createContext, useContext, useState  } from "react";
 
-const ThemeContext = createContext({
-    theme: 'light',
-    toggleTheme: () => {}
-})
+const ThemeContext = createContext(undefined)
 
 export const ThemeProvider = ({ children }) => {
     const [theme, setTheme] = useState('light')
